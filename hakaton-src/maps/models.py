c4ba0c7fa94b795f1601be0e763ad7dd610b8b55
faradj <faradj.musaev@gmail.com>
2016-12-03 03:34:23 +0600

    Created models for other objects

--- hakaton-src/maps/models.py
+++ hakaton-src/maps/models.py
@@ -4,8 +4,32 @@ from django.db import models
 
 
 class Challenge(models.Model):
-    address = map_fields.AddressField(max_length=200)
-    geolocation = map_fields.GeoLocationField(max_length=100)
+    address = map_fields.AddressField(max_length=200, verbose_name=u'Address')
+    geolocation = map_fields.GeoLocationField(max_length=100, verbose_name=u'Geolocation')
+    description = models.TextField(max_length=255, verbose_name=u'Description')
+    reward_point = models.IntegerField(max_length=255, verbose_name=u'Reward Point')
+    difficulty = models.IntegerField(max_length=200, verbose_name=u'Difficulty')
+    photo = models.FileField(verbose_name=u'Photo')
 
     def __str__(self):
         return self.address
+
+
+class Comment(models.Model):
+    content = models.TextField(max_length=255, verbose_name=u'Content')
+    date_created = models.DateField(verbose_name=u'Date created')
+    challenge = models.ForeignKey(Challenge)
+
+    def __str__(self):
+        return self.id
+
+
+class CompletedChallenge(models.Model):
+    challenge = models.ForeignKey(Challenge)
+    user = models.ForeignKey(Profile)
+
+
+class AcceptedChallenge(models.Model):
+    challenge = models.ForeignKey(Challenge)
+    user = models.ForeignKey(Profile)
+
